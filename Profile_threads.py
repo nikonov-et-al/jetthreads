c8a4f2fd4b52ffd7f922a7feed9fc52784836f3e
Aleksei S. Nikonov <72255690+nikonov-et-al@users.noreply.github.com>
2021-03-16 23:00:48 +0300

    Thread selection GUI

--- Profile_threads.py
+++ Profile_threads.py
@@ -1,168 +1,183 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import glob
+from matplotlib.patches import Rectangle
 
 foldername = 'fit_params'
 names = np.sort(glob.glob(foldername + '/fit_*.dat'))
-print(names[0])
 n_slices = len(names)
 im_scale = 0.15
 
-positions = [[] for i in range(n_slices)]
+positions_y = []
 i = 0
+
+positions_x = []
 for name in names:
     data = open(name, 'r')
     data_open = data.readlines()
     data_lines = (data_open[-1]).split('\t')
     data.close()
     parameters = np.array(data_lines).astype(float)
-    positions[i] = parameters[1::3]
+    positions_y.extend(parameters[1::3])
+    x = int(name[15:-4])*im_scale
+    positions_x.extend([x for i in range(len(parameters[1::3]))])
     i = i + 1
 
-x_axis = -np.arange(0, n_slices, 1) * im_scale
+x_1 = positions_x.copy()
+y_1 = positions_y.copy()
+x_2 = []
+y_2 = []
+x_3 = []
+y_3 = []
 
-fig, ax = plt.subplots(figsize = (16,4),dpi = 150)
-j = 0
-for x in x_axis:
-    x_pos = [x for i in range(len(positions[j]))]
-    ax.scatter(x_pos, positions[j], marker = '.', color = 'w')
-    j = j + 1
+fig, ax = plt.subplots(figsize = (10, 7),dpi = 150)
 
-ax.set_aspect('equal')
-ax.set_ylim(-25,25)
-ax.set_xlim(-20, 400)
-ax.set_ylabel('Transverse offset (mas)')
-ax.set_xlabel('Distance along jet (mas)')
-ax.set_title('Space-Ground 3-Gaussian quasi-manual approach.')
+lims = 0
 
-#####################################
-from matplotlib.colors import LogNorm
-from scipy import ndimage
-from matplotlib.offsetbox import AuxTransformBox
-from matplotlib.patches import Ellipse
-import openfits as op
-from matplotlib import rcParams
-from matplotlib.offsetbox import AnchoredOffsetbox
-i_map, i_head = op.fits_open('/home/nikonalesheo/data/M87-Radioastron/space_min.fits')
-rotation_angle = 23
-i_map = ndimage.rotate(i_map, rotation_angle,reshape=True)
-sigma = 1.9*10**(-4)
-jet = np.ma.masked_less(i_map, sigma) ###mad_std
+def main_plot(ax = ax):
+    global lims
+    global x_1, x_2, x_3, y_1, y_2, y_3 
 
-map_size = np.shape(i_map)
+    ax.scatter(x_1, y_1, marker = '.', lw = 1, label = 'No 1')
+    if len(x_2) >0:
+        ax.scatter(x_2, y_2, marker = '.', lw = 1, label = 'No 2')
+    if len(x_3) >0:
+        ax.scatter(x_3, y_3, marker = '.', lw = 1, label = 'No 3')
+    
+    ax.set_aspect('equal')
+    ax.set_ylim(-25,25)
+    ax.set_xlim(-20 + lims, 100 + lims)
+    ax.set_ylabel('Transverse offset (mas)')
+    ax.set_xlabel('Distance along jet (mas)')
+    ax.set_title('Space-Ground 3-Gaussian quasi-manual approach.')
+    ax.legend()
+    plt.draw()
 
+main_plot()
 
-arg_max = np.unravel_index(np.argmax(i_map, axis=None), i_map.shape)
-##########
-#a, b - SEMI - axes of the beam, not axes. in pixels obtained from header.
-a = i_head['BMAJ']*60*60*1000 / im_scale /2
-b = i_head['BMIN']*60*60*1000 / im_scale /2
-#k - inclination coefficient of tangent(kasatelnaya) to the ellipce y = kx + c
-k = np.tan(np.deg2rad( -90 + rotation_angle - i_head['BPA']))
-c = np.sqrt(a**2 * k**2 + b**2)
-#beam is distance between two parallel lines of tangent
-beam = abs(2*c) / (np.sqrt(k**2 + 1**2))
+coords = []
+selected_x = []
+selected_y = []
+selection = False
+thread_num = 2
 
-# Below is beam size to jet axis projetction. 
-k = np.tan(np.deg2rad( -90 + rotation_angle - i_head['BPA'] + 90))
-c = np.sqrt(a**2 * k**2 + b**2)
-beam_jet = abs(2*c) / (np.sqrt(k**2 + 1**2))
+def select(event):
+    
+    global coords
+    global fig, ax
+    global thread_num
 
-rcParams['font.family'] = 'Roboto'
+    #print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+    #          ('double' if event.dblclick else 'single', event.button,
+    #event.x, event.y, event.xdata, event.ydata))
+    
+    if selection == True:
+        if len(coords) == 0:
+            ax.scatter(event.xdata, event.ydata, marker = '+')
+            plt.draw()
+            
+        elif len(coords) == 2:
+            xy = (np.min([coords[0], event.xdata]),
+                    np.min([coords[1], event.ydata]))
+            xy_max = (np.max([coords[0], event.xdata]),
+                    np.max([coords[1], event.ydata]))
+            delta_x = abs(coords[0] - event.xdata)
+            delta_y = abs(coords[1] - event.ydata)
+            ax.add_patch(Rectangle(xy, delta_x, delta_y,
+                        edgecolor = 'black',
+                        fill=False,
+                        lw=2))
+            plt.draw()
+                    
+            x_vals = np.array(x_1)
+            y_vals = np.array(y_1)
+            
+            
+            inds = np.arange(np.min( np.where(x_vals > xy[0]) ), 
+                            np.max( np.where(x_vals < xy_max[0])))
+            for i in inds:
+                if y_vals[i] > xy[1] and y_vals[i] < xy_max[1]:
+                    selected_x.append(x_vals[i])
+                    selected_y.append(y_vals[i])
+            for i in range(len(selected_x)):
+                x_1.remove(selected_x[i])
+                y_1.remove(selected_y[i])
 
-FONT_SIZE = 27
+        if len(coords) >= 4:
+            coords = []
+            plt.cla()
+            main_plot()
+        coords.extend([event.xdata, event.ydata])
+          
 
-rcParams.update({'font.size': FONT_SIZE})
-#########
+def keyboard(event):
+    global coords
+    global selected_x
+    global selected_y
+    global selection
+    global lims 
+    global thread_num
 
-class AnchoredEllipse(AnchoredOffsetbox):
-    def __init__(self, transform, width, height, angle, loc,
-        pad=0.1, borderpad=0.1, prop=None, frameon=True):
-        """
-        Draw an ellipse the size in data coordinate of the give axes.
+    if event.key == ' ':
+        print('The Selection was started.')
+        selected_y = []
+        selected_x = []
+        selection = True
+    if event.key == 'enter':
+        print('The Selection was ended. All relusts were saved.')
+        print('Thread No ', thread_num)
+        selection = False
+        coords = []
+        if thread_num == 1:
+            x_1.extend(selected_x)
+            y_1.extend(selected_y)
+        elif thread_num == 2:
+            x_2.extend(selected_x) 
+            y_2.extend(selected_y)
+        elif thread_num == 3:
+            x_3.extend(selected_x)
+            y_3.extend(selected_y)
 
-        pad, borderpad in fraction of the legend font size (or prop)
-        """
-        self._box = AuxTransformBox(transform)
-        self.ellipse = Ellipse((0, 0), width, height, angle, fc = 'grey')
-        self._box.add_artist(self.ellipse)
-        super().__init__(loc, pad=pad, borderpad=borderpad,
-                child=self._box, prop=prop, frameon=frameon)
+        plt.cla()
+        main_plot()
 
+    if event.key == '1':
+        print('The first thread is in process.')
+        thread_num = 1
+    if event.key == '2':
+        print('The second thread is in process.')
+        thread_num = 2
+    if event.key == '3':
+        print('The third thread is in process.')
+        thread_num = 3
+    
+    if event.key == 'right':
+        plt.cla()
+        lims = lims + 30
+        main_plot()
+    if event.key == 'left':
+        plt.cla()
+        lims = lims - 30
+        main_plot()
 
-
-def draw_ellipse(ax, head):
-    """
-    Draw an ellipse of width=0.1, height=0.15 in data coordinates
-    """
-    ae = AnchoredEllipse(ax.transData,
-    width=head['BMIN']*60*60*1000,
-    height=head['BMAJ']*60*60*1000,
-    angle=-head['BPA'] + rotation_angle,
-    loc='lower left', pad=0.1, borderpad=0.1,
-    frameon=True)
-
-    ax.add_artist(ae)
-
-
-def geomspace(start,finish,sqrt = None):
-    """ 
-    creates geometrical sequence of needed interval.
-
-    Keyword arguments:
-
-    start -- int -- start of sequence. Nonzero!
-    finish -- int -- finish of sequence.
-    sqrt = None -- don`t remember.
-
-    Exmaple:
-
-    geomspace(1, 100)
-
-    """
-    import math
-
-    if sqrt == 'true':
-        n = int(math.floor(1+math.log(finish/start,np.sqrt(2))))
-    else:
-        n = int(round(1+np.log2(finish/start)))
-    y = np.empty(n)
-    x = start
-    i=0
-    while i<n:
-        y[i] = x
-        x = x*np.sqrt(2)
-        i = i+1
-    return y
-
-
-####################################
-
-contours_min =  5*np.min(jet)/np.max(jet)
-levels = geomspace(contours_min ,1, 'true')*np.max(jet)
-
-sky_size = map_size[0]*im_scale/2
-
-shift = (np.array([arg_max[1], arg_max[0]]) - np.array(map_size)/2) * im_scale * [1, -1]
-hor_min = -(-sky_size - shift[0])
-hor_max = -(sky_size - shift[0])
-ver_min = -sky_size + shift[1]
-ver_max = sky_size + shift[1]
-
-ax.contour(i_map.data, levels, colors = 'black', linewidths = 0.8,  
-           extent = [hor_min, hor_max, ver_min, ver_max])
-
-ax.imshow(jet, norm = LogNorm(vmin = np.min(jet)/5, 
-           vmax = np.max(jet)), cmap = 'CMRmap', origin = 'lower',                          extent = [hor_min, hor_max, ver_min, ver_max]
-                   )
-
-ax.set_xlim(20, -450)
-ax.set_ylim(-50, 50)
-ax.set_facecolor('black')
-draw_ellipse(ax, i_head)
-
-fig.savefig('3_gauss.pdf')
+    if event.key == 'e':
+        print('All results saved to files: thread_*.npy')
+        with open('thread_1.npy', 'wb') as f:
+            np.savez(f, x_1, y_1)
+        with open('thread_2.npy', 'wb') as f:
+            np.savez(f, x_2, y_2)
+        with open('thread_3.npy', 'wb') as f:
+            np.savez(f, x_3, y_3)
 
 
 
+
+sel_f = fig.canvas.mpl_connect('button_release_event', select)
+#fig.canvas.mpl_disconnect(sel_f)
+
+key_f = fig.canvas.mpl_connect('key_release_event', keyboard)
+#fig.canvas.mpl_disconnect(key_f)
+
+
+plt.show()
 
